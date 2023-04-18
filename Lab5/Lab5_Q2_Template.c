bec18be90cc58c4162834a4c48c5afe87f668020
kenneth <122667859+kennethlemuel@users.noreply.github.com>
2023-04-19 00:53:59 +0800

    Add files via upload

---
+++ Lab5/Lab5_Q2_Template.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void quickSort(int A[], int low, int high);
+int partition (int A[], int low, int high);
+
+void dualSortedSearch (int[],int, int, int[]);
+
+int main()
+{
+    int i,size,K;
+    int* A;
+    int index[2] = {-1,-1};
+
+    //Search key
+    scanf("%d",&K);
+
+    //create an array
+    scanf("%d",&size);
+    A=(int *)malloc(sizeof(int)*size);
+    for(i=0; i<size; i++)
+    {
+        scanf("%d",&A[i]);
+    }
+
+    quickSort(A,0,size-1);
+    dualSortedSearch(A,size,K,index);
+
+    if(index[0]!=-1)
+        printf("%d %d\n",index[0], index[1]);
+    else
+        printf("Not found");
+
+    free(A);
+    return 0;
+}
+
+void dualSortedSearch(int A[], int size, int K, int dualIndex[])
+{
+   // Write your code here
+   int left, right, current_sum; //initializing the left right and sum variables
+
+   left = 0 ; //setting the left from zero
+   right = size - 1; //setting the right starting from the max number.
+
+   while(i<j){ //for as long as i is smaller than j, then theres a chance that left + right = k
+       if(current_sum = left + right == K){ // if left + right = k then..
+            dualIndex[0] = left;
+            dualIndex[1] = right;
+            return;
+        }
+        else if(current_sum < K){ //if the sum is still smaller than k, we simply add onto the left
+            left++;
+        }
+        else{// if it is too large, we make the number smaller.
+            right--;
+        }
+   }
+}
+
+int partition (int A[], int low, int high)
+{
+    int pivot = A[high];
+    int i = (low - 1);
+
+    int temp;
+    for (int j = low; j <= high- 1; j++)
+    {
+        if (A[j] < pivot)
+        {
+            i++;
+
+            temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+    }
+
+    temp = A[i+1];
+    A[i+1]= A[high];
+    A[high] = temp;
+    return (i + 1);
+}
+
+void quickSort(int A[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = partition(A, low, high);
+
+        quickSort(A, low, pivot - 1);
+        quickSort(A, pivot + 1, high);
+    }
+}
